d4d5747beecc2383ca5870c3e8604ddfae1a75b6
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-08 12:28:09 +0200

    First try

---
+++ src/projektas1/Projektas1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektas1;
+
+/**
+ *
+ * @author krist
+ */
+public class Projektas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!!!");
+        //sveikiesiems skaiciams
+        byte b = 123;
+        short s = 123;
+        int i = 123;
+        long l = 654;
+        //saugomi skaiciai su kableliais
+        float f = 0.2f;
+        double d;
+        //Boolean
+        boolean bool;
+        //Viena vienintele raide
+        char c = 'a';
+        //skaiciai 3 laispniu
+        int sk = 5;
+        
+        sk =sk * sk * sk;
+        
+        System.out.println(sk);
+        
+  
+        
+        
+    }
+    
+}
