4de428a6c1650ebcb56372ccb7145d34c2c06edf
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-08 18:13:53 +0200

    Random numbers

---
+++ src/projektas1/randomCup.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projektas1;
+
+/**
+ *
+ * @author krist
+ */
+public class randomCup {
+     public static void main(String[] args) {
+        //Trying how it works
+        System.out.println("Random number between 0 and 1 : " + Math.random()); 
+        System.out.println("Random number between 0 and 1 : " + Math.random());
+        //
+        System.out.println("Random value between 1 and 10 : " + getRandomInteger(10, 1));
+        System.out.println("Random value between 1 and 10 : " + getRandomInteger(10, 1));
+     }
+     public static int getRandomInteger(int maximum, int minimum){
+         return ((int) (Math.random()*(maximum - minimum))) + minimum;
+     }
+
+}
